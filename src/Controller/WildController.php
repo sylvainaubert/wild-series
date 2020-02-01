2572e339b32c5019fea8e18f25894b03c261e6c6
Sylvain Aubert <sylvain@atelierdixdouze.com>
2020-02-01 14:03:59 +0100

    create show method with slug

--- src/Controller/WildController.php
+++ src/Controller/WildController.php
@@ -2,18 +2,51 @@
 
 namespace App\Controller;
 
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
+
+/**
+ * Class WildController
+ * @package App\Controller
+ *
+ * @Route("/wild")
+ */
 class WildController extends AbstractController
 {
     /**
-     * @Route("/wild", name="wild")
+     * @Route("/", name="wild")
      */
     public function index()
     {
         return $this->render('wild/index.html.twig', [
             'wildseries' => 'Wild Series',
         ]);
+    }
+
+    /**
+     *
+     * @Route("/show/{slug<^[a-z0-9-]+$>}",
+     *     defaults={"slug" = null},
+     *     name="wild_show")
+     * @param string $slug
+     * @return Response
+     */
+    public function show(?string $slug){
+
+        if (!$slug) {
+            $slug = 'Aucune série sélectionnée, veuillez choisir une série';
+        }
+
+        $slug = preg_replace(
+            '/-/',
+            ' ', ucwords(trim(strip_tags($slug)), "-")
+        );
+
+        return $this->render('wild/show.html.twig', [
+            'slug' => $slug,
+        ]);
+
     }
 }
