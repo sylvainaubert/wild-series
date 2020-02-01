44cff7ab032765ede0093c548cc7602f7f86229c
Sylvain Aubert <sylvain@atelierdixdouze.com>
2020-02-01 19:01:11 +0100

    modify show method to show 1 program by Title

--- src/Controller/WildController.php
+++ src/Controller/WildController.php
@@ -2,6 +2,9 @@
 
 namespace App\Controller;
 
+use App\Entity\Category;
+use App\Entity\Program;
+use App\Repository\CategoryRepository;
 use App\Repository\ProgramRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -17,7 +20,7 @@ use Symfony\Component\Routing\Annotation\Route;
 class WildController extends AbstractController
 {
     /**
-     * @Route("/", name="wild")
+     * @Route("/", name="wild_index")
      */
     public function index(ProgramRepository $programs)
     {
@@ -34,7 +37,10 @@ class WildController extends AbstractController
      * @param string $slug
      * @return Response
      */
-    public function show(?string $slug){
+    public function show(?string $slug)
+    {
+
+        /* Code for Quest 5
 
         if (!$slug) {
             $slug = 'Aucune série sélectionnée, veuillez choisir une série';
@@ -48,6 +54,33 @@ class WildController extends AbstractController
         return $this->render('wild/show.html.twig', [
             'slug' => $slug,
         ]);
+        */
+
+
+        if (!$slug) {
+            throw $this
+                ->createNotFoundException('No slug has been sent to find a program in program\'s table.');
+        }
+
+        $slug = preg_replace(
+            '/-/',
+            ' ', ucwords(trim(strip_tags($slug)), "-")
+        );
+
+        $program = $this->getDoctrine()
+            ->getRepository(Program::class)
+            ->findOneBy(['title' => mb_strtolower($slug)]);
+        if (!$program) {
+            throw $this->createNotFoundException(
+                'No program with ' . $slug . ' title, found in program\'s table.'
+            );
+        }
+
+        return $this->render('wild/show.html.twig', [
+            'slug' => $slug,
+            'program' => $program,
+        ]);
 
     }
+
 }
